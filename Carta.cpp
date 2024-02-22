5180e501dcf9ce4167d179b771baffe333882b4e
Yennifer017 <yenniferdelion@gmail.com>
2024-02-21 23:25:45 -0600

    cartas

---
+++ Carta.cpp
@@ -0,0 +1,5 @@
+//
+// Created by igriega on 21/02/24.
+//
+
+#include "Carta.h"
---
+++ Carta.h
@@ -0,0 +1,23 @@
+//
+// Created by igriega on 21/02/24.
+//
+
+#ifndef SOLITARIO_CARTA_H
+#define SOLITARIO_CARTA_H
+
+
+#include <string>
+
+class Carta {
+private:
+    char value;
+    std::string simbol;
+    char color;
+    bool reveal;
+
+public:
+
+};
+
+
+#endif //SOLITARIO_CARTA_H
