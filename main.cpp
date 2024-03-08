d751b1dab8b9aed4ea531c433820d3b9f9c06822
Yennifer017 <yenniferdelion@gmail.com>
2024-03-07 21:42:40 -0600

    main-menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-#include "Container.h"
 #include "Solitaire.h"
+#include "Util.h"
 int main() {
-    Solitaire* solitaire = new Solitaire();
-    solitaire->play();
+    Util* util = new Util();
+    while(true) {
+        Solitaire *solitaire = new Solitaire();
+        solitaire->play();
+        delete(solitaire);
+        std::cout<<"\nDeseas jugar otra vez?   1->Si  2->No \n";
+        int option = util->getNaturalNumber(1, 2);
+        if(option == 2){
+            break;
+        }
+    }
+    delete(util);
 }
